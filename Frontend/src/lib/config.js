b50b8a2c44f0ac1223beb411c74f179bb273b279
Magamed-7 <teachermaga7@gmail.com>
2026-07-31 22:47:10 +0500

    add api endpoint configuration

---
+++ Frontend/src/lib/config.js
@@ -0,0 +1,3 @@
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL || "http://127.0.0.1:8001";
+export const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+export const WS_URL = import.meta.env.VITE_WS_URL || "ws://127.0.0.1:8002";
